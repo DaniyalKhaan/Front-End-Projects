459126342c725e227a8407866af9e6fc128bc8c0
Daniyal <daniyalkhan112001@gmail.com>
2024-03-01 03:57:28 +0500

    increase-Score-Done

--- Bubble-Game/script.js
+++ Bubble-Game/script.js
@@ -17,15 +17,44 @@ document.querySelector(".bubble").onClick = function (){
     console.log("You are going wright");
 }
 }
-
+let hitVal = 0;
 function makeNewHit(){
-    let hitVal = 0;
     hitVal = Math.floor(Math.random() * 10);
-    document.getElementById("hitVal").innerHTML = hitVal;
+    document.getElementById("hitVal").textContent = hitVal;
     // document.querySelector("#hitVal").innerHTML = hitVal;
 }
 
+function runTimer (){
+    let timer = 3;
+    let timerInterval = setInterval(function(){
+        if (timer > 0){
+            timer--;
+            document.querySelector("#timerVal").textContent = timer;
+        } else {
+            clearInterval(timerInterval);
+        }
+    }, 1000)
+}
+
+
+
+let score = 0;
+function increaseScore () {
+    document.getElementById("bottomPanel")
+    .addEventListener("click", function(details){
+       let bubbleClicked = Number(details.target.textContent);
+       if (hitVal == bubbleClicked){
+        score += 10;
+        document.getElementById("scoreVal").textContent = score;
+       }
+    })
+}
+
+
+
+runTimer();
 makeBubbles();
 makeNewHit();
 makeNewHit();
 makeNewHit();
+increaseScore();
