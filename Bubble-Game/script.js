da7ca5e759b92f7df722fb6324d250f09fc2e546
Daniyal <daniyalkhan112001@gmail.com>
2024-02-29 18:43:31 +0500

    Hit-Value-Done

--- Bubble-Game/script.js
+++ Bubble-Game/script.js
@@ -1,4 +1,5 @@
-
+function makeBubbles(){
+    let hit = Math.floor(Math.random() * 10);
 let bubbles = "";
 
 
@@ -9,3 +10,22 @@ for ( let i =0; i<=174; i++){
 }
 
 document.querySelector("#bottomPanel").innerHTML = bubbles;
+document.getElementsByClassName("box").innerHTML = hit;
+
+
+document.querySelector(".bubble").onClick = function (){
+    console.log("You are going wright");
+}
+}
+
+function makeNewHit(){
+    let hitVal = 0;
+    hitVal = Math.floor(Math.random() * 10);
+    document.getElementById("hitVal").innerHTML = hitVal;
+    // document.querySelector("#hitVal").innerHTML = hitVal;
+}
+
+makeBubbles();
+makeNewHit();
+makeNewHit();
+makeNewHit();
